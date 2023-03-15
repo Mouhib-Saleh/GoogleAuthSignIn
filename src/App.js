b04c047312f9601dba2486c693e23f244c36a8cc
mouhib saleh <mouhib.saleh20@gmail.com>
2023-03-15 18:36:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import jwt_decode from 'jwt-decode';
+
+
 
 function App() {
+
+const [user,setUser] = useState({});
+var [hideButton, setHideButton] = useState(true);
+
+ function handelCallbackResponse(response){
+ var object_user = jwt_decode(response.credential);
+ setUser(object_user);
+ setHideButton(false);
+ console.log("pressed log in "+hideButton);
+ } 
+
+ function handleSignOut(e){
+setUser({});
+setHideButton(true);
+console.log(hideButton);
+google.accounts.id.renderButton(
+  document.getElementById("signInDiv"),
+  {theme: "outline",size: "large"}
+)
+ }
+
+useEffect(()=>{
+/* global google */
+google.accounts.id.initialize({
+  client_id:'enterurClientID',
+  callback: handelCallbackResponse
+})
+google.accounts.id.renderButton(
+  document.getElementById("signInDiv"),
+  {theme: "outline",size: "large"}
+)
+},[]);
+
+
+useEffect(()=>{
+  if(hideButton){  google.accounts.id.renderButton(
+    document.getElementById("signInDiv"),
+    {theme: "outline",size: "large"}
+  )}
+
+},[hideButton]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {  hideButton &&
+        <div id="signInDiv"></div>
+      }
+      
+      { user && 
+      <div className='info'>
+        <img src = {user.picture}></img>
+        <h3>{user.name}</h3>
+      </div>
+      }
+      {
+        !hideButton &&
+      <button onClick={(e)=>handleSignOut(e)}>Sign Out</button>
+    }
     </div>
   );
 }
